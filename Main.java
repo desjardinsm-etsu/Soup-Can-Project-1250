6cad686608020a2bed0c35c23c702dd62d9c761b
Mathew Desjardins <desjardinsm@etsu.edu>
2021-10-20 11:14:22 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String[] ing = {"CHicken", "Water", "Salt", "Grease", "Hydrojkfdsayuifewqjhklvcxuiojhklfewqxcjhkfdsa"};
+
+        SoupCan can = new SoupCan(10, 2, "Campbells", 110, 24, 6, ing);
+
+        can.Open();
+        if(can.isOpen())
+        {
+            System.out.println("The can is open!");
+        }
+        else
+        {
+            System.out.println("The can is closed!");
+        }
+
+        can.Close();
+
+        if(can.isOpen())
+        {
+            System.out.println("The can is open!");
+        }
+        else
+        {
+            System.out.println("The can is closed!");
+        }
+
+    }
+}
